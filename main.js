6c9e8dfac3eef28e4de548629c7b09f6307d7c61
Nuria <nurborgen@alu.edu.gva.es>
2023-10-06 19:02:45 +0200

    con 2 funciones bien

--- main.js
+++ main.js
@@ -2,18 +2,24 @@
 async function main() {
 let id = prompt("Introduce tu ID de usuario:")
 
-const URLinit = ' https://jsonplaceholder.typicode.com/users'
+const URLinit = ' https://jsonplaceholder.typicode.com/'
 
-let URL = URLinit + '?id=' + id
-const result = await pideDatos(URL)
+let URLuserById = URLinit + 'users/' + id
+const result = await userById(URLuserById)
 console.log(result)
 
 
 console.log(`ID: ${result.id} nick: ${result.username} nombre: ${result.name}`)
  
+let URLTareasPorUser = URLinit + 'todos?userId=' + id
+const resultTareasPorUser = await tareasPorUser(URLTareasPorUser)
+console.log(resultTareasPorUser)
 
+let URLTareaStatusChange = URLinit + 'todos/' + id
+const resultTareaStatusChange = await tareaStatusChange(URLTareaStatusChange)
+console.log(resultTareaStatusChange)
 
-async function pideDatos(URL) {
+async function userById(URL) {
   const response = await fetch(URL)
   if(!response.ok) {
     throw 'Error ' + response.status + ' de la BBDD: ' + response.statusText
@@ -21,5 +27,26 @@ async function pideDatos(URL) {
   const datos = await response.json()
   return datos
 }
+
+async function tareasPorUser(URL) {
+  const response = await fetch(URL)
+  if(!response.ok) {
+    throw 'Error ' + response.status + ' de la BBDD: ' + response.statusText
+  }
+  const datos = await response.json()
+  return datos
+}
+
+async function tareaStatusChange(URL) {
+  const response = await fetch(URL, {
+    method: 'DELETE',
+  })
+  if(!response.ok) {
+    throw 'Error ' + response.status + ' de la BBDD: ' + response.statusText
+  }
+  const datos = await response.json()
+  return datos
+}
+
 }
 main()

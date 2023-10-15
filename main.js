168d12933de6f09fa2cd21005edc519c93de617e
Nuria <nurborgen@alu.edu.gva.es>
2023-10-15 22:37:46 +0200

    exercici 4.1

--- main.js
+++ main.js
@@ -15,9 +15,19 @@ let URLTareasPorUser = URLinit + 'todos?userId=' + id
 const resultTareasPorUser = await tareasPorUser(URLTareasPorUser)
 console.log(resultTareasPorUser)
 
-let URLTareaStatusChange = URLinit + 'todos/' + id
-const resultTareaStatusChange = await tareaStatusChange(URLTareaStatusChange)
-console.log(resultTareaStatusChange)
+let idDelete = prompt("Introduce el id de la tarea que quieres borrar:")
+let URLTareaDelete = URLinit + 'todos/' + idDelete
+const resultTareaDelete = await tareasDelete(URLTareaDelete)
+console.log(resultTareaDelete)
+
+let idStatusChange = prompt("Introduce el id de la tarea que quieres cambiar a completada:")
+let URLStatusChange = URLinit + 'todos/' + idStatusChange
+const resultStatusChange = await tareasStatusChange(URLStatusChange)
+console.log(resultStatusChange)
+
+let titleTarea = prompt("Introduce el titula de la nueva tarea:")
+const resultURLnewTarea = await newTarea(titleTarea)
+console.log(resultURLnewTarea)
 
 async function userById(URL) {
   const response = await fetch(URL)
@@ -37,7 +47,7 @@ async function tareasPorUser(URL) {
   return datos
 }
 
-async function tareaStatusChange(URL) {
+async function tareasDelete(URL) {
   const response = await fetch(URL, {
     method: 'DELETE',
   })
@@ -46,6 +56,43 @@ async function tareaStatusChange(URL) {
   }
   const datos = await response.json()
   return datos
+}
+
+async function tareasStatusChange(URL) {
+  const response = await fetch(URL, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      completed: true,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+
+  if(!response.ok) {
+    throw 'Error ' + response.status + ' de la BBDD: ' + response.statusText
+  }
+  const datos = await response.json()
+  return datos
+}
+
+async function newTarea(title) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'POST',
+      body: JSON.stringify({
+          userId: id,
+          title: title,
+          completed: false
+      }),
+      headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+      },
+  })
+  if (!response.ok) {
+      throw `Error ${response.status} en la API: ${response.statusText}`
+  }
+  const myData = await response.json()
+  return myData
 }
 
 }
